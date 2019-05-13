57eaaf59dbf4d68631265a786b88202fa2363545
homedweller <50560990+homedweller@users.noreply.github.com>
2019-05-13 08:49:27 +0700

    Add files via upload

---
+++ ProgramATM/frmAccount.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAccount
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.BtnUpdate = New System.Windows.Forms.Button()
+        Me.TxtQty = New System.Windows.Forms.TextBox()
+        Me.TxtWSID = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.BtnLogout = New System.Windows.Forms.Button()
+        Me.BtnAccount = New System.Windows.Forms.Button()
+        Me.BtnRefresh = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(584, 183)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'BtnUpdate
+        '
+        Me.BtnUpdate.Location = New System.Drawing.Point(521, 252)
+        Me.BtnUpdate.Name = "BtnUpdate"
+        Me.BtnUpdate.Size = New System.Drawing.Size(75, 23)
+        Me.BtnUpdate.TabIndex = 1
+        Me.BtnUpdate.Text = "Update"
+        Me.BtnUpdate.UseVisualStyleBackColor = True
+        '
+        'TxtQty
+        '
+        Me.TxtQty.Location = New System.Drawing.Point(496, 226)
+        Me.TxtQty.Name = "TxtQty"
+        Me.TxtQty.Size = New System.Drawing.Size(100, 20)
+        Me.TxtQty.TabIndex = 2
+        '
+        'TxtWSID
+        '
+        Me.TxtWSID.Location = New System.Drawing.Point(496, 200)
+        Me.TxtWSID.Name = "TxtWSID"
+        Me.TxtWSID.Size = New System.Drawing.Size(100, 20)
+        Me.TxtWSID.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(454, 203)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(36, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "WSID"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(415, 229)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(75, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Quantity Uang"
+        '
+        'BtnLogout
+        '
+        Me.BtnLogout.Location = New System.Drawing.Point(12, 203)
+        Me.BtnLogout.Name = "BtnLogout"
+        Me.BtnLogout.Size = New System.Drawing.Size(75, 23)
+        Me.BtnLogout.TabIndex = 6
+        Me.BtnLogout.Text = "Logout"
+        Me.BtnLogout.UseVisualStyleBackColor = True
+        '
+        'BtnAccount
+        '
+        Me.BtnAccount.Location = New System.Drawing.Point(12, 232)
+        Me.BtnAccount.Name = "BtnAccount"
+        Me.BtnAccount.Size = New System.Drawing.Size(75, 23)
+        Me.BtnAccount.TabIndex = 7
+        Me.BtnAccount.Text = "ATM"
+        Me.BtnAccount.UseVisualStyleBackColor = True
+        '
+        'BtnRefresh
+        '
+        Me.BtnRefresh.Location = New System.Drawing.Point(440, 252)
+        Me.BtnRefresh.Name = "BtnRefresh"
+        Me.BtnRefresh.Size = New System.Drawing.Size(75, 23)
+        Me.BtnRefresh.TabIndex = 8
+        Me.BtnRefresh.Text = "Refresh"
+        Me.BtnRefresh.UseVisualStyleBackColor = True
+        '
+        'frmAccount
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(608, 278)
+        Me.Controls.Add(Me.BtnRefresh)
+        Me.Controls.Add(Me.BtnAccount)
+        Me.Controls.Add(Me.BtnLogout)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TxtWSID)
+        Me.Controls.Add(Me.TxtQty)
+        Me.Controls.Add(Me.BtnUpdate)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "frmAccount"
+        Me.Text = "Account Saldo"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents BtnUpdate As System.Windows.Forms.Button
+    Friend WithEvents TxtQty As System.Windows.Forms.TextBox
+    Friend WithEvents TxtWSID As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents BtnLogout As System.Windows.Forms.Button
+    Friend WithEvents BtnAccount As System.Windows.Forms.Button
+    Friend WithEvents BtnRefresh As System.Windows.Forms.Button
+End Class
